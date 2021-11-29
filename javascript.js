14aa5df32203c99194c948a410df5e64da3c8d94
colinjamesfirth <colinjamesfirth@gmail.com>
2021-11-29 11:54:05 +0000

    Tabl is now more responsive, supports variable width columns has has a more predictable behaviour for auto column data on the selectable column

--- javascript.js
+++ javascript.js
@@ -1,45 +1,106 @@
 $(document).ready(function() {
 
+  var baseRemPX = 14; //px
+  var cellXPadding = 0.74; //rem
+  var responsiveTableBreakpointSmallTable = 40; //rem;
+
+  var table_column_widths = [
+    //[sizeIndex, Xrem],
+    ['0','0'],
+    ['1','3'],
+    ['2','10'],
+    ['3','15']
+  ];
+
   $('tr[data-row-clickable]').on('click',function() {
     row_name = $(this).attr('data-row-clickable');
     alert('opened ' + row_name);
   });
 
+
+  var table_header = [
+    ['Name',2],
+    ['Gender',2],
+    ['Hair',2],
+    ['Eyes',2],
+    ['Age',1],
+    ['Status',2],
+    ['Town',2],
+    ['Home',2],
+    ['Job',2],
+    ['Narrative',3]
+  ];
   var table_data = [
-    ['Name','Gender','Hair','Eyes','Status','Town','Home','Job','Narative'],
-    ['Martin','Male','Blonde','Blue','Single','Bolton','Terrace','Builder','An amazing worker with brevity that would impress a lion'],
-    ['Julie','Female','Brown','Green','Married','Wigan','Detached','Accountant','If she can\'t add it, it\'s not worth adding'],
-    ['Rey','Non-binary','Silver','Blue','Single','Leigh','Flat','Driver','A nicer person you couldn\'t wish to meet'],
-    ['Jennifer','Female','Black','Brown','Civil Partnership','Huddersfield','Semi','Dentist','The best tooth MOT in Huddersfield']
+    ['Martin','Male','Blonde','Blue','24','Single','Bolton','Terrace','Builder','An amazing worker with brevity that would impress a lion'],
+    ['Julie','Female','Brown','Green','62','Married','Wigan','Detached','Accountant','If she can\'t add it, it\'s not worth adding'],
+    ['Rey','Non-binary','Silver','Blue','42','Single','Leigh','Flat','Driver','A nicer person you couldn\'t wish to meet'],
+    ['Jennifer','Female','Black','Brown','31','Civil Partnership','Huddersfield','Semi','Dentist','The best tooth MOT in Huddersfield']
   ];
 
   var counter = 0;
   var table = '';
 
-  // make the table
-  table_data.forEach( function(r) {
-    if (counter == 0) {
-      output = '<tr>';
-      r.forEach( function(th) {
-        output += '<th>' + th + '</th>\n';
-      });
-      output += '<th data-column-selectable>\n</th>\n<th>Open</th>\n';
-      $('table#data thead').append(output);
-    } else {
-      output = '<tr data-row-clickable data-table-row="' + counter + '">';
+  function makeTableHeader(target) {
+    output = '<thead>\n<tr>';
+    table_header.forEach( function(th) {
+        output += '<th data-column-size="' + th[1] + '">' + th[0] + '</th>\n';
+    });
+    output += '<th data-column-selectable>\n</th>\n<th>Open</th>\n';
+    output += '</tr>\n</thead>\n';
+    $(target).append(output);
+  }
+  makeTableHeader('table#data');
+
+  function makeTableBody(target) {
+    var output = '<tbody>\n';
+    table_data.forEach( function(r) {
+      output += '<tr data-row-clickable data-table-row="' + counter + '">';
       col1_data = r[0];
       r.forEach( function(td) {
         output += '<td>' + td + '</td>\n';
       });
       output += '<td></td>\n<td><button data-open-button aria-label="Open record for ' + col1_data + '">Open</button></td>\n';
-      $('table#data tbody').append(output);
-    }
-    counter ++;
-  });
+      output += '</tr>'
+      counter ++;
+    });
+    output += '</tbody>\n';
+    $(target).append(output);
+  }
+  makeTableBody('table#data');
 
+  function hideDataColumns() {
+    widthTable = $('table#data').outerWidth();
+    widthContainer = $('table#data').closest('.table-container').outerWidth();
+    widthLastColumn = $('table#data thead th:nth-last-of-type(1)').outerWidth();
+    widthSelectColumn = $('table#data thead th:nth-last-of-type(2)').outerWidth();
+    widthTableAvailable = widthContainer - (widthSelectColumn + widthLastColumn);
+    var counter = 0;
+    var sum = 0;
+
+    $('table#data thead th:not(:nth-last-of-type(-n+2))').each( function() {
+      cellWidth = $(this).attr('data-column-size');
+      cellWidth = table_column_widths[cellWidth][1];
+      cellWidth = (cellWidth * baseRemPX) + 1;
+      sum += cellWidth;
+      if (sum > widthTableAvailable) {
+        $(this).hide();
+        $('table#data tbody tr').each( function() {
+          $(this).find('td').eq(counter).hide();
+        })
+      }
+      else {
+        $(this).show();
+        $('table#data tbody tr').each( function() {
+          $(this).find('td').eq(counter).show();
+        })
+      }
+      counter ++;
+    });
+  }
 
   function clearColumnData() {
     select_column_index = $('th[data-column-selectable]').index();
+    $('select[data-column-selected] option[data-choose]').prop('selected', true);
     $('table#data tbody tr').each(function() {
       $(this).find('td:nth-of-type(' + (select_column_index + 1) + ')').empty();
     });
@@ -47,8 +108,11 @@ $(document).ready(function() {
 
   function setColumnData(data_column) {
     select_column_index = $('th[data-column-selectable]').index();
-    counter = 1;
-    $('select[data-column-selected]').find('option[data-choose]').remove();
+    counter = 0;
+    if (data_column == 'auto') {
+      clearColumnData();
+    }
+    $('select[data-column-selected] option[value="' + data_column + '"]').prop('selected', true);
     $('table#data tbody tr').each(function() {
       cell_data = table_data[counter][data_column];
       $(this).find('td:nth-of-type(' + (select_column_index + 1) + ')').text(cell_data);
@@ -56,34 +120,35 @@ $(document).ready(function() {
     });
   }
 
-  function refreshColumnSelect() {
-    if ( $('select[data-column-selected]').length ) {
-      var selected = $('select[data-column-selected]').attr('data-column-selected');
-    } else {
-      var selected = 'auto';
-    }
-    if (selected == 'auto') {
-      var firstHidden = $("table#data thead th:hidden:first").index();
-      options = '';
-      if (firstHidden == -1) {
-        options +='<option data-choose aria-label="Choose the data for this column">Choose</option>\n';
+  function autoColumn() {
+    var data = $('select[data-column-selected]').attr('data-column-selected');
+    $('select[data-column-selected] option[data-choose]').remove();
+    if ( data == 'auto') {
+      if ( $('table#data thead th:nth-child(2)').is(":hidden") ) {
+        setColumnData(1);
+      } else {
         clearColumnData();
+        $('select[data-column-selected]').prepend('<option data-choose value="auto" aria-label="Choose the data for this column" selected>Choose</option>\n');
       }
-      table_data[0].forEach( function(th,i) {
-        var selected = '';
-        if (firstHidden == i) {
-          selected = ' selected';
-          setColumnData(i);
-        }
-        options += '<option value="' + i + '"' + selected + '>' + th + '</option>\n';
-      });
-      var column_select = '<select data-column-selected="auto" aria-label="Choose the data for this column">\n' + options + '</select>\n';
-      $('table#data th[data-column-selectable]').html(column_select);
     }
   }
-  refreshColumnSelect();
+
+  function makeColumnSelect() {
+    var firstHidden = $("table#data thead th:hidden:first").index();
+    options = '';
+    table_header.forEach( function(th,i) {
+      options += '<option value="' + i + '">' + th[0] + '</option>\n';
+    });
+    var column_select = '<select data-column-selected="auto" aria-label="Choose the data for this column">\n' + options + '</select>\n';
+    $('table#data th[data-column-selectable]').html(column_select);
+  }
+
+  hideDataColumns();
+  makeColumnSelect();
+  autoColumn();
   $(window).resize( function() {
-    refreshColumnSelect();
+    hideDataColumns();
+    autoColumn();
   });
 
 
@@ -91,6 +156,7 @@ $(document).ready(function() {
     var data_column = $(this).val();
     $(this).attr('data-column-selected',data_column);
     setColumnData(data_column);
+    $('select[data-column-selected]').find('option[data-choose]').remove();
   });
 
   $('table#data td:not(:last-child), table#data button[data-open-button]').on('click',function() {
@@ -104,7 +170,5 @@ $(document).ready(function() {
     alert('edit: ' + dataCol1Value);
   });
 
-
   // end doc ready
-
 });
